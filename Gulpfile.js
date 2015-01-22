c4dbcf2843374edaef0803f6aa78600a8f3b6a7a
Ryan <ryanjlee12@gmail.com>
2015-01-22 15:00:28 -0800

    (doc) Add gulpfile and basic folders

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp      = require('gulp'),
+    nodemon   = require('gulp-nodemon');
+
+// the paths to our app files
+var paths = {
+  // all our client app js files, not including 3rd party js files
+  scripts: ['client/app/*.js'],
+  html: [],
+  styles: ['client/styles/*.css'],
+  test: ['specs/*.js']
+};
+
+gulp.task();
