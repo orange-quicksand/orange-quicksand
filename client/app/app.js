5cd03664d5d41e0e50bf7e2ea06a53ed96dc4bfa
bcbcb <bcbcb@users.noreply.github.com>
2015-01-23 15:57:02 -0800

    Set up Angular structure

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,18 @@
-angular.module('uGame', [
-  'ui.router',
-  'lumx'
-]);
+var app = angular
+  .module('uGame', [
+    'ui.router',
+    'lumx'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/HomeTemplate.html',
+        controller: 'HomeController'
+      })
+      .state('game', {
+        url: '/game/:id',
+        templateUrl: 'game/GameTemplate.html',
+        controller: 'GameController'
+      });
+  });
