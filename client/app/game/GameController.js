053d92cbfc1fc7d9ff86f089a6625a1ef1bfa57a
Omar Duarte <omarduarte@gmail.com>
2015-01-26 14:48:40 -0800

    Grab gameBoyColor API from iFrame on frame load

--- client/app/game/GameController.js
+++ client/app/game/GameController.js
@@ -1,22 +1,45 @@
 angular.module('uGame.game', [])
 
 .controller('GameController', ['$scope', '$stateParams', 'Game' ,function($scope, $stateParams, Game){
-   
- $scope.loadGame = function() {
- 	Game.get($stateParams.id)
- 	  .success(function(resp) {
+
+  $scope.loadGame = function() {
+    Game.get($stateParams.id)
+      .success(function(resp) {
         console.log(resp);
- 	  })
- 	  .error(function(resp) {
+      })
+      .error(function(resp) {
         console.log(resp);
- 	  });
- };
+      });
+  };
+
+  $scope.getGameBoyAPI = function() {
+    $scope.API = window.frames.GBC.gameBoyAPI;
+    console.dir($scope.API);
+  };
+
+
 }])
 
 .factory('Game', ['$http', function($http) {
   return {
-  	get: function(id) {
-  	  return $http.get('/game/' + id);
-  	}
+    get: function(id) {
+      return $http.get('/game/' + id);
+    }
   };
-}]);
+}])
+
+.directive('iframeOnload', [function(){
+  return {
+
+    scope: {
+      callBack: '&iframeOnload'
+    },
+
+    link: function(scope, element, attrs){
+      element.on('load', function(){
+        return scope.callBack();
+      })
+    }
+
+  }
+}])
