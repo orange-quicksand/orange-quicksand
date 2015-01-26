4a05a7bc87f875f1cd9b1d4102dced2837c9b5bd
bcbcb <bcbcb@users.noreply.github.com>
2015-01-25 23:39:39 -0800

    (feat) Add Docco to Gulp and create initial docs with Docco

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,10 @@ var gulp    = require('gulp');
 var watch   = require('gulp-watch');
 var sass    = require('gulp-sass');
 var jshint  = require('gulp-jshint');
+var docco   = require('gulp-docco');
 
 
-// The paths to our app files
+// The paths to our app's files
 var paths = {
   scripts: [
     './client/app/**/*.js',
@@ -16,17 +17,27 @@ var paths = {
   test: ['./specs/*.js']
 };
 
+// Compile SASS to CSS
 gulp.task('sass', function () {
   gulp.src(paths.styles)
     .pipe(sass())
     .pipe(gulp.dest('./client/styles/'));
 });
 
+// Lint js files using JSHint
 gulp.task('jshint', function () {
   gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
+
+// Build documentation using Docco
+gulp.task('docco', function () {
+  gulp.src(paths.scripts)
+    .pipe(docco())
+    .pipe(gulp.dest('./docs'));
+});
+
 
 // Watch when files change
 gulp.task('watch', function () {
