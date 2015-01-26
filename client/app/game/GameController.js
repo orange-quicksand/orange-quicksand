477d18bb4dfd43f0e93bdb235a9a1ac8eeb0bdad
Omar Duarte <omarduarte@gmail.com>
2015-01-26 14:12:58 -0800

    Add emulator to repo

--- client/app/game/GameController.js
+++ client/app/game/GameController.js
@@ -1,11 +1,22 @@
 angular.module('uGame.game', [])
 
-.controller('GameController', function($scope, $http, $stateParams){
-  $http({
-    method: 'GET',
-    url: '/game' + $stateParams.id
-  })
-  .then(function(resp){
-    console.log(resp);
-  });
-});
+.controller('GameController', ['$scope', '$stateParams', 'Game' ,function($scope, $stateParams, Game){
+   
+ $scope.loadGame = function() {
+ 	Game.get($stateParams.id)
+ 	  .success(function(resp) {
+        console.log(resp);
+ 	  })
+ 	  .error(function(resp) {
+        console.log(resp);
+ 	  });
+ };
+}])
+
+.factory('Game', ['$http', function($http) {
+  return {
+  	get: function(id) {
+  	  return $http.get('/game/' + id);
+  	}
+  };
+}]);
