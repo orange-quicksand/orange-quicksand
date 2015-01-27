5b54c4c8dbcfb6396a67049aa8a20a07d903dfd3
Ryan <ryanjlee12@gmail.com>
2015-01-26 22:49:17 -0800

    (feat) Implemented routing for roms and enabled server to send roms to client

--- client/app/game/GameController.js
+++ client/app/game/GameController.js
@@ -3,7 +3,7 @@ angular.module('uGame.game', [])
 .controller('GameController', function($scope, $http, $stateParams){
   $http({
     method: 'GET',
-    url: '/game' + $stateParams.id
+    url: '/api/game/' + $stateParams.id
   })
   .then(function(resp){
     console.log(resp);
