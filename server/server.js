a01bac26ed1586a03b7a6b301d616166a2abf8cd
Ryan <ryanjlee12@gmail.com>
2015-01-26 22:49:17 -0800

    (feat) Implemented routing for roms and enabled server to send roms to client

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var Game = require('./games/gamesModel.js');
 var Rom = require('./games/romsModel.js');
-var library = require('./library.js');
+var library = require('./gameLibrary.js');
 var romLibrary = require('./romLibrary.js');
 
 var app = express();
@@ -12,18 +12,20 @@ app.use(express.static(__dirname + '/../client'));
 var port = process.env.PORT || 3000;
 app.listen(port);
 
-// create database
+// Create mongo database
 var location = process.env.LOC || 'localhost/orangequicksand';
 mongoose.connect('mongodb://' + location);
 
-// initialize database
+// Initialize database with test game info
 for (var i = 0; i < library.length; i++) {
   new Game(library[i]).save();
 }
 
-new Rom(romLibrary[0]).save();
+for (var i = 0; i < romLibrary.length; i++) {
+  new Rom(romLibrary[i]).save();
+}
 
-// routing for homepage
+// Routing for homepage
 app.get('/api/games', function(req, res){
   Game.find(function(err, results) {
     console.log(results);
@@ -31,7 +33,17 @@ app.get('/api/games', function(req, res){
   });
 });
 
-// routing for game page
+// Determine which game to send
+app.param('code', function(req, res, next, code){
+  req.id = code;
+  next();
+});
+
+// Routing for game page
 app.get('/api/game/:code', function(req, res){
-  
+  var id = req.id;
+  Rom.find({id: id}, function(err, results) {
+    console.log('results: ' + results);
+    res.send(results);
+  });
 });
