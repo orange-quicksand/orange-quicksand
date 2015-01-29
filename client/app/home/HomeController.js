0c315c0f440c1e043f0dc1063171d9a8a0852af1
Mike <realmike33@gmail.com>
2015-01-29 14:03:16 -0800

    minor code cleanup in controllers

--- client/app/home/HomeController.js
+++ client/app/home/HomeController.js
@@ -3,29 +3,7 @@ angular.module('uGame.home', [])
 
 .controller('HomeController', function($scope, $http, $interval, $state, Game, $location) {
 
-  $http({
-    method: 'GET',
-    url: '/api/games'
-  })
-  .then(function(resp){
-    if (resp.data) {
-      $scope.data = resp.data;
-    } else {
-      $location.path('/login');
-    }
-  });
-
-  var array = ['I', 'we', 'They', 'U'];
-  var i = -1;
-
-  $interval(function(){
-    if(i >= array.length){
-      i = -1;
-    }
-    i++;
-    $scope.changedWords = array[i];
-  }, 500, 4);
-
+  //depending on game, your url will match its id from the database
   $scope.goToState = function(id){
    $state.go('game', {id: id});
   };
