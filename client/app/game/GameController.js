b7d8a86ce780b90883c89f61d31119d2ac3c9f6f
Omar Duarte <omarduarte@gmail.com>
2015-01-29 23:41:03 -0800

    (feat) Show game view menu on mousemove and mouseover

--- client/app/game/GameController.js
+++ client/app/game/GameController.js
@@ -7,16 +7,68 @@ angular.module('uGame.game', [])
 //
 // Controller for the Gameboy Color emulator.
 // 
-.controller('GameController', function($scope, $window, $stateParams, $http, Game, $location) {
+
+.controller('GameController', function($scope, $timeout, $stateParams, $location, Game) {
+
   
+  var gameIsLoaded = false;
+  var gameIsPaused = false;
+  var menuTimer = null;
+  var menuHasJustBeenShow = false;
+  var menuIsPinned = false;
+
+  $scope.menuIsShown = false;
+  $scope.gameInfo = {
+    title: 'Loading Game...'
+  };
+
   $scope.loadGame = function() {
-    Game.get($stateParams.id).then(function(game) {
-      if (game) {
-        $scope.API.init(game.rom);
-      } else {
+    Game.get($stateParams.id)
+      .success(function(game) {        
+        $scope.API.init(game[0].rom);
+        $scope.gameInfo = {
+          title: game[0].title
+        };
+        gameIsLoaded = true;
+      })
+      .error(function(error) {
+        console.error(error);
         $location.path('/login');
+      });
+  };
+
+  $scope.togglePinMenu = function() {    
+    menuIsPinned = !menuIsPinned;
+  };
+
+  $scope.showMenuWhileMoving = function(event) {    
+    if (gameIsLoaded && !menuHasJustBeenShow && !menuIsPinned) {
+      
+      $scope.menuIsShown = true;
+
+      if (menuTimer) { 
+        $timeout.cancel(menuTimer); 
       }
-    });      
+      
+      menuTimer = $timeout(function() { 
+        if (!menuIsPinned) {
+          $scope.hideMenu();         
+        }
+      }, 2000);      
+    }
+  };
+  
+  $scope.hideMenu = function() {
+    $scope.menuIsShown = false;
+    menuIsPinned = false;
+
+    // HACK - Omar
+    // If we don't do this, the Menu will flash again.
+    // There's something weird happening with the angular
+    // digest loop that I'm unable to debug.
+    // Try removing this and see for yourself.
+    menuHasJustBeenShow = true;
+    $timeout(function() { menuHasJustBeenShow = false; }, 500);
   };
 
   $scope.getGameBoyAPI = function() {
@@ -35,7 +87,7 @@ angular.module('uGame.game', [])
 // and calls it as a $scope function when the element
 // has loaded.
 //
-.directive('uGameIframeOnload', ['$window', function($window){
+.directive('uGameIframeOnload', function($window){
   var w = angular.element($window);
 
   return {
@@ -50,4 +102,4 @@ angular.module('uGame.game', [])
       });
     }
   };
-}]);
+});
