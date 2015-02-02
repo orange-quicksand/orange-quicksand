4d602e86281f672782c3da2b11e09ea06528f6eb
Ryan <ryanjlee12@gmail.com>
2015-02-02 11:58:07 -0800

    (Feat) Add new games to libaries

--- server/gameLibrary.js
+++ server/gameLibrary.js
@@ -1,38 +1,75 @@
 module.exports = [
   {
     id: '0',
+    title: 'Super Mario Land',
+    image: 'SuperMarioLand.jpg',
+    description: 'A turtle stomping plumber who looks for his woman'
+
+  },
+  {
+    id: '1',
+    title: 'Super Mario Bros Deluxe',
+    image: 'SuperMarioBrosDeluxe.jpg',
+    description: 'A turtle stomping plumber who looks for his woman - deluxe'
+  },
+   {
+    id: '2',
+    title: 'Tetris',
+    image: 'Tetris.jpg',
+    description: 'Cram blocks together'
+  },
+  {
+    id: '3',
+    title: 'The Legend of Zelda: Links Awakening DX',
+    image: 'LoZLinksAwakeningDX.jpg',
+    description: 'A boy who finds a sword and breaks pots'
+  },
+   {
+    id: '4',
+    title: 'The Legend of Zelda: Oracle of Ages',
+    image: 'LoZAges.jpg',
+    description: 'A boy finds more pots to break'
+  },
+  {
+    id: '5',
     title: 'Pokemon: Blue Version',
     image: 'PokemonBlue.jpg',
     description: 'A boy who enslaves cute monsters'
   },
   {
-    id: '1',
-    title: 'Super Mario Land',
-    image: 'SuperMarioLand.jpg',
-    description: 'A turtle stomping plumber who looks for his woman'
+    id: '6',
+    title: 'Pokemon: Crystal Version',
+    image: 'PokemonCrystal.jpg',
+    description: 'A boy who enslaves twice the amount of cute monsters than the blue version'
   },
   {
-    id: '2',
-    title: 'The Legend of Zelda: Links Awakening',
-    image: 'LoZLinksAwakening.jpg',
-    description: 'A boy who finds a sword and breaks pots'
+    id: '7',
+    title: 'Harvest Moon 3',
+    image: 'HarvestMoon3.jpg',
+    description: 'Boy meets plants, boy plants plant'
   },
    {
-    id: '3',
-    title: 'The Legend of Zelda: Oracle of Ages',
-    image: 'LoZAges.jpg',
-    description: 'A boy finds more pots to break'
+    id: '8',
+    title: 'Mario Tennis',
+    image: 'MarioTennis.jpg',
+    description: 'Plumbers vs Princesses.. in tennis'
   },
-   {
-    id: '4',
-    title: 'Tetris',
-    image: 'Tetris.jpg',
-    description: 'Cram blocks together'
+  {
+    id: '9',
+    title: 'Tetris Attack',
+    image: 'TetrisAttack.jpg',
+    description: 'Switch blocks instead of cramming them'
   },
-   {
-    id: '5',
-    title: 'Proxima',
-    image: 'Proxima.jpg',
-    description: 'Space game with space ships'
+  {
+    id: '10',
+    title: 'Metroid II',
+    image: 'MetroidII.jpg',
+    description: 'Girl in suit shoots photon balls'
+  },
+  {
+    id: '11',
+    title: 'Bomberman GB',
+    image: 'BombermanGB.jpg',
+    description: 'A suspicious looking dude with tons of bombs'
   }
 ];
