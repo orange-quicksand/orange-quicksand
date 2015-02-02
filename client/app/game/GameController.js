8e5d2a6abf288ee2fcfd91325254b9885ee69985
Omar Duarte <omarduarte@gmail.com>
2015-02-02 10:56:09 -0800

    (Feat) Add dialog that shows Keyboard Controller mappings`

--- client/app/game/GameController.js
+++ client/app/game/GameController.js
@@ -8,7 +8,7 @@ angular.module('uGame.game', [])
 // Controller for the Gameboy Color emulator.
 // 
 
-.controller('GameController', function($scope, $timeout, $stateParams, $location, $document, LxNotificationService, Game) {
+.controller('GameController', function($scope, $timeout, $stateParams, $location, $document, LxNotificationService, LxDialogService, Game) {
 
   var gameIsLoaded = false;
   var gameIsPaused = false;
@@ -19,15 +19,21 @@ angular.module('uGame.game', [])
   var state;
 
   // ALL KEY BINDINGS HERE
-  var keyboardControllerKeys = {
-    '39': 'right',
-    '37': 'left',
-    '38': 'up',
-    '40': 'down',
+  //-------------------------------------
+  //
+  // Names of known key codes (0-255)
+  var keyboardMap = ["","","","CANCEL","","","HELP","","BACK_SPACE","TAB","","","CLEAR","ENTER","RETURN","","SHIFT","CONTROL","ALT","PAUSE","CAPS_LOCK","KANA","EISU","JUNJA","FINAL","HANJA","","ESCAPE","CONVERT","NONCONVERT","ACCEPT","MODECHANGE","SPACE","PAGE_UP","PAGE_DOWN","END","HOME","LEFT","UP","RIGHT","DOWN","SELECT","PRINT","EXECUTE","PRINTSCREEN","INSERT","DELETE","","0","1","2","3","4","5","6","7","8","9","COLON","SEMICOLON","LESS_THAN","EQUALS","GREATER_THAN","QUESTION_MARK","AT","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","WIN","","CONTEXT_MENU","","SLEEP","NUMPAD0","NUMPAD1","NUMPAD2","NUMPAD3","NUMPAD4","NUMPAD5","NUMPAD6","NUMPAD7","NUMPAD8","NUMPAD9","MULTIPLY","ADD","SEPARATOR","SUBTRACT","DECIMAL","DIVIDE","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","F13","F14","F15","F16","F17","F18","F19","F20","F21","F22","F23","F24","","","","","","","","","NUM_LOCK","SCROLL_LOCK","WIN_OEM_FJ_JISHO","WIN_OEM_FJ_MASSHOU","WIN_OEM_FJ_TOUROKU","WIN_OEM_FJ_LOYA","WIN_OEM_FJ_ROYA","","","","","","","","","","CIRCUMFLEX","EXCLAMATION","DOUBLE_QUOTE","HASH","DOLLAR","PERCENT","AMPERSAND","UNDERSCORE","OPEN_PAREN","CLOSE_PAREN","ASTERISK","PLUS","PIPE","HYPHEN_MINUS","OPEN_CURLY_BRACKET","CLOSE_CURLY_BRACKET","TILDE","","","","","VOLUME_MUTE","VOLUME_DOWN","VOLUME_UP","","","SEMICOLON","EQUALS","COMMA","MINUS","PERIOD","SLASH","BACK_QUOTE","","","","","","","","","","","","","","","","","","","","","","","","","","","OPEN_BRACKET","BACK_SLASH","CLOSE_BRACKET","QUOTE","","META","ALTGR","","WIN_ICO_HELP","WIN_ICO_00","","WIN_ICO_CLEAR","","","WIN_OEM_RESET","WIN_OEM_JUMP","WIN_OEM_PA1","WIN_OEM_PA2","WIN_OEM_PA3","WIN_OEM_WSCTRL","WIN_OEM_CUSEL","WIN_OEM_ATTN","WIN_OEM_FINISH","WIN_OEM_COPY","WIN_OEM_AUTO","WIN_OEM_ENLW","WIN_OEM_BACKTAB","ATTN","CRSEL","EXSEL","EREOF","PLAY","ZOOM","","PA1","WIN_OEM_CLEAR",""];
+  
+  // Controller keys.
+  $scope.keyboardControllerKeys = {
     '88': 'a',
     '90': 'b',
     '16': 'select',
-    '13': 'start'
+    '13': 'start',
+    '39': 'right',
+    '37': 'left',
+    '38': 'up',
+    '40': 'down'
   };
 
   // This var is used for a Hack in $scope.hideMenu()
@@ -186,17 +192,29 @@ angular.module('uGame.game', [])
     menuHasJustBeenShow = true;
     $timeout(function() { menuHasJustBeenShow = false; }, 800);
   };
+  
 
+  // CONTROLLER
+  //------------------
+  //
   $scope.onKeyDown = function(key) {
-    if (keyboardControllerKeys.hasOwnProperty(key)) {      
-      $scope.API.keyDown(keyboardControllerKeys[key]);
+    if ($scope.keyboardControllerKeys.hasOwnProperty(key)) {      
+      $scope.API.keyDown($scope.keyboardControllerKeys[key]);
     }
   };
 
   $scope.onKeyUp = function(key) {
-    if (keyboardControllerKeys.hasOwnProperty(key)) {
-      $scope.API.keyUp(keyboardControllerKeys[key]);
+    if ($scope.keyboardControllerKeys.hasOwnProperty(key)) {
+      $scope.API.keyUp($scope.keyboardControllerKeys[key]);
     }
+  };
+
+  $scope.openControllerDialog = function(dialog) {
+    LxDialogService.open(dialog);
+  };
+
+  $scope.fromCharCodeToString = function(code) {
+    return keyboardMap[code];
   };
 
 })
